279ea9b163993dd9ba28b761226eb5388c0101f8
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-03 10:52:46 -0500

    Enabled IPv6 for VPC and public subnets

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "vpc" {
-  cidr_block = var.vpc_cidr_block
+  cidr_block                       = var.vpc_cidr_block
+  assign_generated_ipv6_cidr_block = true
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -7,24 +8,38 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "public_a" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.public_subnet_a
-  availability_zone = var.subnet_az_a
-  map_public_ip_on_launch = true
+  vpc_id                          = aws_vpc.vpc.id
+
+  cidr_block                      = var.public_subnet_a
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 1)
+  assign_ipv6_address_on_creation = true
+
+  availability_zone               = var.subnet_az_a
+  map_public_ip_on_launch         = true
+  
 }
 
 resource "aws_subnet" "public_b" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.public_subnet_a
-  availability_zone = var.subnet_az_b
-  map_public_ip_on_launch = true
+  vpc_id                          = aws_vpc.vpc.id
+
+  cidr_block                      = var.public_subnet_a
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 2)
+  assign_ipv6_address_on_creation = true
+
+  availability_zone               = var.subnet_az_b
+  map_public_ip_on_launch         = true
 }
 
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.igw.id
   }
 }
 
