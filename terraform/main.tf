9c2f332fa142ecb02b9a0fb1d5aff6a4f43ad39b
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-08 14:36:04 -0500

    inputting vpc outputs into endpoints module instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,38 @@
 module "banking_vpc" {
-  source = "./modules/vpc"
+  source           = "./modules/vpc"
 
-  vpc_cidr_block = "10.10.0.0/16"
+  vpc_cidr_block   = "10.10.0.0/16"
 
-  public_subnet_a = "10.10.1.0/24"
-  public_subnet_b = "10.10.2.0/24"
+  public_subnet_a  = "10.10.1.0/24"
+  public_subnet_b  = "10.10.2.0/24"
 
   private_subnet_a = "10.10.101.0/24"
   private_subnet_b = "10.10.102.0/24"
 
-  subnet_az_a = "us-east-1a"
-  subnet_az_b = "us-east-1b"
+  subnet_az_a      = "us-east-1a"
+  subnet_az_b      = "us-east-1b"
 }
 
 module "payments_vpc" {
-  source = "./modules/vpc"
+  source           = "./modules/vpc"
 
-  vpc_cidr_block = "10.20.0.0/16"
+  vpc_cidr_block   = "10.20.0.0/16"
 
-  public_subnet_a = "10.20.1.0/24"
-  public_subnet_b = "10.20.2.0/24"
+  public_subnet_a  = "10.20.1.0/24"
+  public_subnet_b  = "10.20.2.0/24"
 
   private_subnet_a = "10.20.101.0/24"
   private_subnet_b = "10.20.102.0/24"
 
-  subnet_az_a = "us-east-1a"
-  subnet_az_b = "us-east-1b"
+  subnet_az_a      = "us-east-1a"
+  subnet_az_b      = "us-east-1b"
+}
+
+module "vpc_endpoints" {
+  source                 = "./modules/endpoints"
+
+  vpc_id                 = module.vpc.vpc_id
+  private_route_table_id = module.vpc.private_route_table_id
+  private_subnet_a       = module.vpc.private_subnet_a
+  private_subnet_b       = module.vpc.private_subnet_b
 }
