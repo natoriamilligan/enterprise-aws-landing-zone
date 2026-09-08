c88fd679cae0d36ea077767eb9fdd7bff17fe45d
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-08 14:22:29 -0500

    created outputs for VPC module

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value       = aws_vpc.vpc.id
+}
+
+output "private_route_table_id" {
+  value       = aws_route_table.private_route_table.id
+}
+
+output "private_subnet_a" {
+  value       = aws_subnet.private_a.id
+}
+
+output "private_subnet_b" {
+  value       = aws_subnet.private_b.id
+}
