074c22834d1b1fff461b05afa0c3a7dd1eab6824
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-09 14:11:37 -0500

    created 2 ECS service modules for banking and payments VPCs

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,11 +28,64 @@ module "payments_vpc" {
   subnet_az_b      = "us-east-1b"
 }
 
-module "vpc_endpoints" {
+module "banking_vpc_endpoints" {
   source                 = "./modules/endpoints"
 
-  vpc_id                 = module.vpc.vpc_id
-  private_route_table_id = module.vpc.private_route_table_id
-  private_subnet_a       = module.vpc.private_subnet_a
-  private_subnet_b       = module.vpc.private_subnet_b
+  vpc_id                 = module.banking_vpc.vpc_id
+  private_route_table_id = module.banking_vpc.private_route_table_id
+  private_subnet_a       = module.banking_vpc.private_subnet_a
+  private_subnet_b       = module.banking_vpc.private_subnet_b
+}
+
+module "payments_vpc_endpoints" {
+  source                 = "./modules/endpoints"
+
+  vpc_id                 = module.payments_vpc.vpc_id
+  private_route_table_id = module.payments_vpc.private_route_table_id
+  private_subnet_a       = module.payments_vpc.private_subnet_a
+  private_subnet_b       = module.payments_vpc.private_subnet_b
+}
+
+module "banking_ecs_service" {
+  source                       = "./modules/ecs-service"
+
+  ecs_cluster_name             = "banking-cluster"
+  ecs_task_execution_role_name = ecsTaskExecutionRoleBanking
+  ecs_log_group_name           = "/ecs/banking-tasks"
+  ecs_ecs_family_name          = "banking-task-family"
+  ecs_task_cpu                 = "256"
+  ecs_memory                   = "512"
+  ecs_container_name           = "banking-container"
+  ecs_container_cpu            = 0
+  container_port               = 80
+  host_port                    = 80
+  region                       = "us-east-2"
+  app_task_sg                  = banking-task-sg
+  vpc_id                       = module.banking_vpc.vpc_id
+  endpoints_sg                 = module.endpoints.endpoints_sg_id
+  app_service_name             = "banking-service"
+  private_subnet_a             = module.banking_vpc.private_subnet_a
+  private_subnet_b             = module.banking_vpc.private_subnet_b
+}
+
+module "payments_ecs_service" {
+  source                 = "./modules/ecs-service"
+
+  ecs_cluster_name             = "payments-cluster"
+  ecs_task_execution_role_name = ecsTaskExecutionRolePayments
+  ecs_log_group_name           = "/ecs/payments-tasks"
+  ecs_ecs_family_name          = "payments-task-family"
+  ecs_task_cpu                 = "256"
+  ecs_memory                   = "512"
+  ecs_container_name           = "payments-container"
+  ecs_container_cpu            = 0
+  container_port               = 80
+  host_port                    = 80
+  region                       = "us-east-2"
+  app_task_sg                  = payments-task-sg
+  vpc_id                       = module.payments_vpc.vpc_id
+  endpoints_sg                 = module.endpoints.endpoints_sg_id
+  app_service_name             = "payments-service"
+  private_subnet_a             = module.payments_vpc.private_subnet_a
+  private_subnet_b             = module.payments_vpc.private_subnet_b
 }
