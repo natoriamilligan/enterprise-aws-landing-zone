5f36a5a32da752b727755137415e630df90d496e
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-10 16:57:10 -0500

    created nlb target group and listener

--- terraform/modules/private-link/main.tf
+++ terraform/modules/private-link/main.tf
@@ -11,9 +11,20 @@ resource "aws_security_group" "nlb_sg" {
   vpc_id      = var.nlb_vpc
 }
 
-resource "aws_lb_target_group" "test" {
-  name     = "tf-example-lb-tg"
+resource "aws_lb_target_group" "nlb" {
+  name     = var.nlb_tg_name
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "nlb" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nlb.arn
+  }
 }
