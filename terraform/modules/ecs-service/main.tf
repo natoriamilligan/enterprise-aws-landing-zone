84000189aec436ce8ec70fbbec5c87d4c66ad82e
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-10 16:07:43 -0500

    renamed target group name for load balancer

--- terraform/modules/ecs-service/main.tf
+++ terraform/modules/ecs-service/main.tf
@@ -82,7 +82,7 @@ resource "aws_ecs_service" "app-service" {
   }
 
   load_balancer {
-    target_group_arn = var.alb_target_group_arn
+    target_group_arn = var.lb_target_group_arn
     container_name   = var.ecs_container_name
     container_port   = var.container_port
   }
