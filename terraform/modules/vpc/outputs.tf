274a0308f438cf46cdc7d876b5fb95fd90fa0afd
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-11 15:37:19 -0500

    Add CIDR block output to VPC module

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,15 +1,19 @@
 output "vpc_id" {
-  value       = aws_vpc.vpc.id
+  value = aws_vpc.vpc.id
 }
 
 output "private_route_table_id" {
-  value       = aws_route_table.private_route_table.id
+  value = aws_route_table.private_route_table.id
 }
 
 output "private_subnet_a" {
-  value       = aws_subnet.private_a.id
+  value = aws_subnet.private_a.id
 }
 
 output "private_subnet_b" {
-  value       = aws_subnet.private_b.id
+  value = aws_subnet.private_b.id
+}
+
+output "cidr_block" {
+  value = aws_vpc.vpc.cidr_block
 }
