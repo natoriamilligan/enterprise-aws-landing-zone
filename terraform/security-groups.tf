b3c1aad190ec1eb681a7ee76c6f30f2dcadd17c4
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-11 16:37:39 -0500

    Updated security group rules for ALB and NLB
    Updated ingress rules to use 'referenced_security_group_id' and added egress rules for private link

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,16 +1,31 @@
 resource "aws_vpc_security_group_ingress_rule" "allow_alb" {
-  security_group_id = module.banking_ecs_service.sg_id
-  from_port         = 80
-  ip_protocol       = "HTTP"
-  to_port           = 80
-  security_groups   = var.alb_security_group
+  security_group_id              = module.banking_ecs_service.sg_id
+  from_port                      = 80
+  ip_protocol                    = "HTTP"
+  to_port                        = 80
+  referenced_security_group_id   = var.alb_security_group
+}
+
+resource "aws_vpc_security_group_egress_rule" "to_private_link_interface_endpoint" {
+  security_group_id            = module.banking_ecs_service.sg_id
+  from_port                    = var.provider_port
+  protocol                     = "tcp"
+  to_port                      = var.provider_port
+  referenced_security_group_id = module.private_link.private_link_interface_endpoint
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_nlb" {
-  security_group_id = module.payments_ecs_service.sg_id
-  from_port         = var.provider_port
-  ip_protocol       = "tcp"
-  to_port           = var.provider_port
-  security_groups   = module.private-link.nlb_sg
+  security_group_id              = module.payments_ecs_service.sg_id
+  from_port                      = var.provider_port
+  ip_protocol                    = "tcp"
+  to_port                        = var.provider_port
+  referenced_security_group_id   = module.private-link.nlb_sg
 }
 
+resource "aws_vpc_security_group_egress_rule" "to_provider" {
+  security_group_id            = module.private_link.nlb_sg
+  from_port                    = var.provider_port
+  ip_protocol                  = "tcp"
+  to_port                      = var.provider_port
+  referenced_security_group_id = module.payments_ecs_service.sg_id
+}
