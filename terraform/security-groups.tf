108cbee4a69b34248da117d608b8d5ddb1e97757
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-11 15:54:05 -0500

    Update security group ingress rules for provider ECS service

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,7 +1,16 @@
 resource "aws_vpc_security_group_ingress_rule" "allow_alb" {
-  security_group_id = var.ecs_security_group_id
+  security_group_id = module.banking_ecs_service.sg_id
   from_port         = 80
   ip_protocol       = "HTTP"
   to_port           = 80
   security_groups   = var.alb_security_group
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_nlb" {
+  security_group_id = module.payments_ecs_service.sg_id
+  from_port         = var.provider_port
+  ip_protocol       = "tcp"
+  to_port           = var.provider_port
+  security_groups   = module.private-link.nlb_sg
+}
+
