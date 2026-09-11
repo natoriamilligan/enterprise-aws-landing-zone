0c0b10f921f696f38fdf273a07e213ad9a580bbc
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-11 15:12:39 -0500

    Renamed security group for private link endpoint

--- terraform/modules/private-link/main.tf
+++ terraform/modules/private-link/main.tf
@@ -53,28 +53,28 @@ resource "aws_vpc_endpoint" "private_link" {
   vpc_endpoint_type = "Interface"
 }
 
-resource "aws_security_group" "private_link_endpoint" {
+resource "aws_security_group" "private_link_interface_endpoint" {
   name        = var.vpc_endpoint_sg_name
   vpc_id      = var.consumer_vpc
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ecs" {
-  security_group_id = aws_security_group.private_link_endpoint.id
+  security_group_id = aws_security_group.private_link_interface_endpoint.id
   from_port         = var.provider_port
   ip_protocol       = "tcp"
   to_port           = var.provider_port
-  security_groups   = var.ecs_security_group
+  security_groups   = var.consumer_ecs_security_group
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all" {
-  security_group_id = aws_security_group.private_link_endpoint.id
+  security_group_id = aws_security_group.private_link_interface_endpoint.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
 
 resource "aws_vpc_endpoint_security_group_association" "private_link" {
   vpc_endpoint_id   = aws_vpc_endpoint.private_link.id
-  security_group_id = aws_security_group.private_link_endpoint.id
+  security_group_id = aws_security_group.private_link_interface_endpoint.id
 }
 
 resource "aws_vpc_endpoint_subnet_association" "private_link_private_a" {
