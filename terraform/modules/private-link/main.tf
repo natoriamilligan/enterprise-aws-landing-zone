4446382ae746934e73e973f8f660d8b34f318cf2
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-11 10:34:22 -0500

    created private link interface endpoint for consumer vpc
    Updated VPC ID for NLB and added resources for private link.

--- terraform/modules/private-link/main.tf
+++ terraform/modules/private-link/main.tf
@@ -15,7 +15,7 @@ resource "aws_lb_target_group" "nlb" {
   name     = var.nlb_tg_name
   port     = 80
   protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  vpc_id   = var.nlb_vpc
 }
 
 resource "aws_lb_listener" "nlb" {
@@ -37,4 +37,29 @@ resource "aws_vpc_endpoint_service" "nlb" {
 resource "aws_vpc_endpoint_service_allowed_principal" "authorized_acct" {
   vpc_endpoint_service_id = aws_vpc_endpoint_service.nlb.id
   principal_arn           = var.allowed_principal_arn
+}
+
+resource "aws_vpc_endpoint" "private_link" {
+  vpc_id            = var.consumer_vpc
+  service_name      = aws_vpc_endpoint_service.nlb.service_name
+  vpc_endpoint_type = "Interface"
+  
+  private_dns_enabled = true
+
+  policy = data.aws_iam_policy_document.endpoint_service_policy.json
+}
+
+resource "aws_vpc_endpoint_security_group_association" "private_link" {
+  vpc_endpoint_id   = aws_vpc_endpoint.private_link.id
+  security_group_id = aws_security_group.private_link.id
+}
+
+resource "aws_vpc_endpoint_subnet_association" "private_link_private_a" {
+  vpc_endpoint_id = aws_vpc_endpoint.private_link.id
+  subnet_id       = var.private_subnet_a
+}
+
+resource "aws_vpc_endpoint_subnet_association" "private_link_private_b" {
+  vpc_endpoint_id = aws_vpc_endpoint.private_link.id
+  subnet_id       = var.private_subnet_b
 }
