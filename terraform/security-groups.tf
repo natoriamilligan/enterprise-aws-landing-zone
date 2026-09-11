9a7b51cfee379ab57ef160745a6fe48a655cc5a6
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-11 15:18:13 -0500

    Update security group variable for ALB ingress rule

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_security_group_ingress_rule" "allow_alb" {
-  security_group_id = aws_security_group.app_task_sg.id
+  security_group_id = var.ecs_security_group_id
   from_port         = 80
   ip_protocol       = "HTTP"
   to_port           = 80
