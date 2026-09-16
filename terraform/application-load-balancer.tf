3d401bfe39666ede6650aab9c90176f128f5b660
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-16 15:57:15 -0500

    Added ALB target group and listener resources

--- terraform/application-load-balancer.tf
+++ terraform/application-load-balancer.tf
@@ -13,6 +13,29 @@ resource "aws_lb" "alb" {
   ]
 }
 
+resource "aws_lb_target_group" "alb_tg" {
+  name        = "alb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.banking_vpc.vpc_id
+
+  health_check {
+    path        = "/home"
+  }
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
+
 resource "aws_vpc_security_group_ingress_rule" "alb_http" {
   security_group_id = aws_security_group.alb_sg.id
   cidr_ipv4         = "0.0.0.0/0"
