4bff5d5f8a130db8628836edc4bc25d18eb679ec
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-16 10:43:31 -0500

    Add outputs for cluster and service names

--- terraform/modules/ecs-service/outputs.tf
+++ terraform/modules/ecs-service/outputs.tf
@@ -1,3 +1,11 @@
 output "sg_id" {
   value = aws_security_group.app_task_sg.id
 }
+
+output "cluster_name" {
+  value = aws_ecs_cluster.app_cluster.name
+}
+
+output "service_name" {
+  value = aws_ecs_service.app-service.name
+}
