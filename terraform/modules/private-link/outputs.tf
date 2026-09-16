d9fd199bfbdaec0dd8295aa0852cb4fa1ce31a55
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-16 11:03:58 -0500

    Add outputs for private link and NLB configurations

--- terraform/modules/private-link/outputs.tf
+++ terraform/modules/private-link/outputs.tf
@@ -2,6 +2,22 @@ output "nlb_sg" {
   value = aws_security_group.nlb_sg.id
 }
 
-output "private_link_interface_endpoint" {
+output "private_link_interface_endpoint_sg" {
   value = aws_security_group.private_link_interface_endpoint.id
+}
+
+output "private_link_interface_endpoint_id" {
+  value = aws_vpc_endpoint.private_link.id
+}
+
+output "private_link_endpoint_service_id" {
+  value = aws_vpc_endpoint_service.nlb.id
+}
+
+output "nlb_arn_suffix" {
+  value = aws_lb.nlb.arn_suffix
+}
+
+output "nlb_target_group_arn_suffix" {
+  value = aws_lb.nlb.arn_suffix
 }
