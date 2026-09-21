0e3f21460a42a49cce5bdffc68be93c93983510e
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-09-21 13:02:22 -0500

    added images for ecs services

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ module "banking_ecs_service" {
   ecs_task_cpu                 = "256"
   ecs_memory                   = "512"
   ecs_container_name           = "banking-container"
-  image_name                   = TODO
+  image_name                   = "${aws_ecr_repository.banking.repository_url}:latest"
   ecs_container_cpu            = 0
   container_port               = var.provider_port
   host_port                    = var.provider_port
@@ -69,7 +69,7 @@ module "banking_ecs_service" {
   app_service_name             = "banking-service"
   private_subnet_a             = module.banking_vpc.private_subnet_a
   private_subnet_b             = module.banking_vpc.private_subnet_b
-  lb_target_group_arn         = aws_lb_listener.alb_listener.arn
+  lb_target_group_arn          = aws_lb_listener.alb_listener.arn
 }
 
 module "payments_ecs_service" {
@@ -82,7 +82,7 @@ module "payments_ecs_service" {
   ecs_task_cpu                 = "256"
   ecs_memory                   = "512"
   ecs_container_name           = "payments-container"
-  image_name                   = TODO
+  image_name                   = "${aws_ecr_repository.payments.repository_url}:latest"
   ecs_container_cpu            = 0
   container_port               = var.provider_port
   host_port                    = var.provider_port
@@ -118,15 +118,15 @@ module "private_link" {
 module "monitoring" {
   source                      = "./modules/monitoring"
 
-  banking_cluster_name = module.banking_ecs_service.cluster_name
-  banking_service_name = module.banking_ecs_service.service_name
-  payments_cluster_name = module.payments_ecs_service.cluster_name
-  payments_service_name = module.payments_ecs_service.service_name
-  email_address = natoriaray@utexas.edu
-  endpoint_id = module.private_link.private_link_interface_endpoint_id
-  endpoint_service_id = module.private_link.private_link_endpoint_service_id
-  alb_arn_suffix = aws_lb.alb.arn_suffix
+  banking_cluster_name        = module.banking_ecs_service.cluster_name
+  banking_service_name        = module.banking_ecs_service.service_name
+  payments_cluster_name       = module.payments_ecs_service.cluster_name
+  payments_service_name       = module.payments_ecs_service.service_name
+  email_address               = natoriaray@utexas.edu
+  endpoint_id                 = module.private_link.private_link_interface_endpoint_id
+  endpoint_service_id         = module.private_link.private_link_endpoint_service_id
+  alb_arn_suffix              = aws_lb.alb.arn_suffix
   alb_target_group_arn_suffix = aws_lb_target_group.alb_tg.arn_suffix
-  nlb_arn_suffix = module.private_link.nlb_arn_suffix
+  nlb_arn_suffix              = module.private_link.nlb_arn_suffix
   nlb_target_group_arn_suffix = module.private_link.nlb_target_group_arn_suffix
 }
